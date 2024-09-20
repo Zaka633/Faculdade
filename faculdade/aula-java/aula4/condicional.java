b7b55fcff7c3186ae96dbdb2ab1f04f96a662884
Zaka633 <rodrigues20ryan@gmail.com>
2024-09-20 00:21:04 -0300

    mudança de num1 to num3 para teste

--- faculdade/aula-java/aula4/condicional.java
+++ faculdade/aula-java/aula4/condicional.java
@@ -7,14 +7,14 @@ public class condicional {
         
         Scanner teclado = new Scanner(System.in);
 
-
+        //mudei num1 para num3 para teste, mas não faz diferença alguma!
 
         System.out.println("digite o primeiro número:");
-        int num1 = teclado.nextInt();
+        int num3 = teclado.nextInt();
 
         System.out.println("digite o primeiro número:");
         int num2 =teclado.nextInt();
-        System.out.println("o maior número é:"+(num1>num2?num1:num2));
+        System.out.println("o maior número é:"+(num3>num2?num3:num2));
 
         teclado.close();
 
